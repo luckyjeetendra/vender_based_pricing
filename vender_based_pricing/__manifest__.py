09691e6263e4fa066b43316289231553b56f1a11
dell <luckyjeetendra@gmail.com>
2023-01-01 23:55:10 +0530

    Added icon and banner

--- vender_based_pricing/__manifest__.py
+++ vender_based_pricing/__manifest__.py
@@ -39,13 +39,13 @@
     """,
 
     'author': "Jeetendra S",
-    'website': "http://www.yourcompany.com",
+    # 'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory/Purchase',
-    'version': '0.1',
+    'version': '15.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'product', 'purchase', 'purchase_stock'],
@@ -61,4 +61,8 @@
     'demo': [
         'demo/demo.xml',
     ],
+    'images': ['static/description/banner.jpg']
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
